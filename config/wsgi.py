e61fc2409b9fb2257ffaaf61f2bedcdc3656befc
Akobir Marupov <akobirdilmurodovich@gmail.com>
2025-07-18 17:33:35 +0500

    .

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+
+# STATIC fayllarni serve qilish uchun dj-static qo‘shamiz
+from dj_static import Cling, MediaCling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
-application = get_wsgi_application()
+application = Cling(MediaCling(get_wsgi_application()))
