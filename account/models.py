f046a336dc79dee42b8b53e4da7e6a798b1f67bb
AkobirEdso <marupovakobir7@gmail.com>
2025-07-23 19:18:06 +0500

    assalom

--- account/models.py
+++ account/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from common.models import BaseModel
-from django.utils.translation import gettext_lazy as _
-from django.utils import timezone
-from datetime import timedelta
+# from django.utils.translation import gettext_lazy as _
+# from django.utils import timezone
+# from datetime import timedelta
 
 from account.manager import UserManager
 
